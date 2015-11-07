4065bc206716a6db6f6efca657ac1e278e1c9da7
RayHuo <rayhuo_81@hotmail.com>
2015-11-07 15:26:38 +0800

    添加心情fragment

--- ToDoList/src/com/ray/todolist/items/MoodFragment.java
+++ ToDoList/src/com/ray/todolist/items/MoodFragment.java
@@ -1,7 +1,9 @@
 package com.ray.todolist.items;
 
 import com.ray.todolist.R;
+import com.ray.todolist.db.DataBaseHelper;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +12,10 @@ import android.view.ViewGroup;
 
 public class MoodFragment extends Fragment {
 
+	private DataBaseHelper mDBHelper = null;
+	private SQLiteDatabase mSQLDB = null;
+	private final String TABLENAME = "moods";
+	
 	private View rootView = null;
 	
 	@Override
@@ -17,6 +23,32 @@ public class MoodFragment extends Fragment {
 			Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.mood_layout, container, false);
 		
+		initDataBase();
+		initView();
+		prepareListener();
+		
 		return rootView;
+	}
+	
+	
+	private void initDataBase() {
+		mDBHelper = new DataBaseHelper(getActivity());
+		mSQLDB = mDBHelper.getReadableDatabase();		// 仅能读的数据库
+	}
+	
+	
+	private void initView() {
+		
+	}
+	
+	private void prepareListener() {
+		
+	}
+	
+	
+	@Override
+	public void onDestroy() {
+		mSQLDB.close();
+		super.onDestroy();
 	}
 }
